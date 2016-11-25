0cb9212d0f2127d256488af4fa3146b2c9524a7d
Stephen.Corrigan <stephen.corrigan@live.co.uk>
2016-11-25 11:45:21 -0500

    adding procfile requirements.txt and updated setting

--- harmony/settings.py
+++ harmony/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -51,6 +52,8 @@ INSTALLED_APPS = [
     'disqus',
     'styleme',
     'django_gravatar',
+
+
     # 'django-gravatar2',
 ]
 
@@ -92,12 +95,13 @@ WSGI_APPLICATION = 'harmony.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-
+CLEARDB_DATABASE_URL=os.environ.get("CLEARDB_DATABASE_URL","")
+DATABASES['default']= dj_database_url.parse(CLEARDB_DATABASE_URL)
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
---
+++ readme.md
@@ -0,0 +1,3 @@
+Created by Stephen Corrigan
+
+Harmony Styling site for user to read blogs and upload images of clothes for the stylist to put together for user

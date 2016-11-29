abd10e2c7ab325c7128ce9289404c537df99444c
Stephen.Corrigan <stephen.corrigan@live.co.uk>
2016-11-29 16:26:10 -0500

    fixed uploading file via submit button

--- harmony/settings.py
+++ harmony/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['harmony-styling.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['harmony-styling.herokuapp.com', '127.0.0.1']
 
 
 STRIPE_PUBLISHABLE = os.getenv('STRIPE_PUBLISHABLE')
@@ -95,13 +95,13 @@ WSGI_APPLICATION = 'harmony.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 
-CLEARDB_DATABASE_URL=os.environ.get("CLEARDB_DATABASE_URL","")
-DATABASES['default']= dj_database_url.parse(CLEARDB_DATABASE_URL)
+# CLEARDB_DATABASE_URL=os.environ.get("CLEARDB_DATABASE_URL","")
+# DATABASES['default']= dj_database_url.parse(CLEARDB_DATABASE_URL)
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
